d70a95904c6b4f346910047a45fbd9c1713cf990
김현우 <125671828+khw274@users.noreply.github.com>
2026-01-16 14:49:00 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -0,0 +1,13 @@
+def solution(my_string, is_suffix):
+    answer = []
+    
+    list(my_string)
+    
+    for i in range(len(my_string)):
+        answer.append(my_string[i:])
+        
+    if is_suffix in answer:
+        return 1
+    else:
+        return 0
+    
